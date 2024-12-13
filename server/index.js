384ca0e9293b03f589d83bf868aa29f0cb6b4230
nishantMirrar <nishant@mirrar.com>
2024-12-14 01:41:33 +0530

    Enhance CORS configuration to allow all origins and specific HTTP methods

--- server/index.js
+++ server/index.js
@@ -11,7 +11,13 @@ import taskRoutes from './routes/taskRoutes.js';
 
 const app=express();
 
-app.use(cors());  
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 app.use(express.json()); 
 
 app.use((err, req, res, next) => {
