a343181bacdba233c1d8bb97fb74ee7f826de5ee
nishantMirrar <nishant@mirrar.com>
2024-12-14 11:14:23 +0530

    Restrict CORS origin to localhost for development environment

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app=express();
 
 app.use(
   cors({
-    origin: "*",
+    origin:'http://localhost:5173',
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
